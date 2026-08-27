ff043f271897096de665296a517e506ec229f27d
pk1012 <121967246+pk1012@users.noreply.github.com>
2026-08-28 03:05:44 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "workout-tracker-v44";
+const CACHE_NAME = "workout-tracker-v49";
 
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./css/styles.css?v=1.7.44",
-  "./css/components.css?v=1.7.44",
-  "./css/responsive.css?v=1.7.44",
+  "./css/styles.css?v=1.7.45",
+  "./css/components.css?v=1.7.48",
+  "./css/responsive.css?v=1.7.45",
   "./js/data.js",
   "./js/workouts.js",
   "./js/exercises.js",
